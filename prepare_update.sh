75be4c1282a15c8f6162c4f2e9ede03eb5a52fa2
vectra130 <vectra130>
2016-04-11 21:45:12 +0200

    initialisiert das update

---
+++ prepare_update.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+#Updatefile zum updaten des StreamingClients
+
+UPDATEDIR="/etc/vectra130/update/git_update_files"
+GITREPO="https://github.com/Vectra130/streamingclient_update.git"
+
+
+download()
+{
+	#update herunterladen
+	git clone --depth 1 $GITREPO $UPDATEDIR
+
+	# fuehre update aus
+	[ ! -e $UPDATEDIR/update.sh ] && exit 1
+	$UPDATEDIR/update.sh
+	if [ x$? == x0 ]; then
+		echo OK
+	fi
+}
+
+
+download
+
+exit 0
