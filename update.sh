a86d2aaa23b44e43458f7a51e27d4384b2a1644a
vectra130 <vectra130>
2016-06-13 02:27:48 +0200

    update 1.11.2

--- update.sh
+++ update.sh
@@ -6,19 +6,24 @@ SYSTEMDDIR="/etc/systemd/system"
 BINDIR="/etc/vectra130/bin"
 
 #StreamingClient beenden
+logger -t UPDATE beende StreamingClient
 if [ x$(cat /tmp/.frontendSet) != xsuspend ]; then
 	echo stop > /tmp/.frontendSet
 	while [ $(pidof -xs StreamingClient | wc -w) != 0 ]; do
 		sleep 1
+		killall StreamingClient
 	done
 fi
 
+logger -t UPDATE installiere Files
 cd $UPDATEDIR
 ./FILES/scripts/showscreenimage.sh update
 install --mode=755 BootSequenz $BINDIR/
 install --mode=755 StreamingClient $BINDIR/
 install --mode=755 CheckServer $BINDIR/
 cp FILES/streamingclient.service FILES/streamingclient-boot.service $SYSTEMDDIR/
+
+logger -t UPDATE beendet
 
 sleep 2
 exit 0
